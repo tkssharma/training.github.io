8e73b50cdcdf4fc57dfcab9b2557a3a3f8338223
tarun <cooltarunsharma111@gmail.com>
2016-03-14 18:38:58 +0530

    added

--- app/module/bootstrap.js
+++ app/module/bootstrap.js
@@ -31,44 +31,58 @@
  		});
 
 
-myApplication
-.controller(
-		"parentCntl",
-		[
-		 "$scope",
-		 "$rootScope",
-		 "$state",
-		 "$http",
-		 "AuthenticationService",
-		 "USER_ROLES",
-		 '$timeout',
-		 function parentCntl( $scope, $rootScope, $state,
-				 $http,AuthenticationService,USER_ROLES,$timeout) {
+ 	myApplication
+ 	.controller(
+ 		"parentCntl",
+ 		[
+ 		"$scope",
+ 		"$rootScope",
+ 		"$state",
+ 		"$http",
+ 		"AuthenticationService",
+ 		"USER_ROLES",
+ 		'$timeout',
+ 		function parentCntl( $scope, $rootScope, $state,
+ 			$http,AuthenticationService,USER_ROLES,$timeout) {
 
-			 $scope.currentUser = null;
-			 $scope.userRoles = USER_ROLES;
+ 			$scope.currentUser = null;
+ 			$scope.userRoles = USER_ROLES;
 
 
-		 } ])
-		 .constant('AUTH_EVENTS', {
-			 loginSuccess: 'auth-login-success',
-			 loginFailed: 'auth-login-failed',
-			 logoutSuccess: 'auth-logout-success',
-			 sessionTimeout: 'auth-session-timeout',
-			 notAuthenticated: 'auth-not-authenticated',
-			 notAuthorized: 'auth-not-authorized',
-			 loginmessage: ' invalid userid and password',
-			 registermessage: 'unable to register user',
-			 resetmessage: 'Reset password will be sent to your registered email id please check your email'
-		 })
-		 .constant('USER_ROLES', {
-			 all: '*',
-			 admin: 'admin',
-			 guest: 'guest',
-			 host: 'host'
-		 });
+ 		} ])
+ 	.constant('AUTH_EVENTS', {
+ 		loginSuccess: 'auth-login-success',
+ 		loginFailed: 'auth-login-failed',
+ 		logoutSuccess: 'auth-logout-success',
+ 		sessionTimeout: 'auth-session-timeout',
+ 		notAuthenticated: 'auth-not-authenticated',
+ 		notAuthorized: 'auth-not-authorized',
+ 		loginmessage: ' invalid userid and password',
+ 		registermessage: 'unable to register user',
+ 		resetmessage: 'Reset password will be sent to your registered email id please check your email'
+ 	})
+ 	.constant('USER_ROLES', {
+ 		all: '*',
+ 		admin: 'admin',
+ 		guest: 'guest',
+ 		host: 'host'
+ 	})
+ 	.filter('urlsuffix', function() {
 
-		 })();
+ 		return function(number) {
+
+		// Ensure that the passed in data is a number
+		if(number == '' || number == undefined) {
+			// If the data is not a number or is less than one (thus not having a cardinal value) return it unmodified.
+			return number;
+
+		} else {
+			return number+ '&output=embed';
+		}
+	}
+   });
+
+ })();
 
 
 
